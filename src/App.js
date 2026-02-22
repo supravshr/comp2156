32b0420e6f987fe2591d311ada78abacbceef54e
Suprav Shrestha <supravshrestha.72@gmail.com>
2026-02-22 15:54:02 -0500

    Name and ID updated

--- src/App.js
+++ src/App.js
@@ -12,8 +12,8 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1>Studen ID: Put Your Student ID</h1>
-      <h1>Studen ID: Put Your Student Name</h1>
+      <h1>Student ID: 101513435</h1>
+      <h1>Student ID: Suprav Shrestha</h1>
       <h1>Student Information System</h1>
       <h5>Powered By: moxDroid Labs Inc. || DevOps</h5>
       <StudentForm addStudent={addStudent} />
